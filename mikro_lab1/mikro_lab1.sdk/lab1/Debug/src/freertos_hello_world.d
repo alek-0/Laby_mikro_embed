838f6737ce61e18b3221e90f7f04fd13abd36af2
alek-0 <149092624+alek-0@users.noreply.github.com>
2025-10-07 05:33:09 +0000

    dodanie kodu lab 1

---
+++ mikro_lab1/mikro_lab1.sdk/lab1/Debug/src/freertos_hello_world.d
@@ -0,0 +1,56 @@
+src/freertos_hello_world.o src/freertos_hello_world.o: \
+ ../src/freertos_hello_world.c \
+ ../../lab1_bsp/ps7_cortexa9_0/include/FreeRTOS.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/FreeRTOSConfig.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/projdefs.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/portable.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/deprecated_definitions.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/portmacro.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/mpu_wrappers.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/task.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/list.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/queue.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/task.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/timers.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/xparameters.h
+
+../../lab1_bsp/ps7_cortexa9_0/include/FreeRTOS.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/FreeRTOSConfig.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/projdefs.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/portable.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/deprecated_definitions.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/portmacro.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/mpu_wrappers.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/task.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/list.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/queue.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/task.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/timers.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/xparameters.h:
